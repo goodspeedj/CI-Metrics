8a74eefa4a072f4193c8bbf77ff0f5e40e2dc974
Jim Goodspeed <jgoodsp@fas.harvard.edu>
2014-01-28 14:58:43 -0500

    New data files and selecting of the legend working

--- js/fix_time_by_portfolio.js
+++ js/fix_time_by_portfolio.js
@@ -1,4 +1,4 @@
-var main_margin = {top: 20, right: 80, bottom: 100, left: 40},
+var main_margin = {top: 20, right: 80, bottom: 100, left: 100},
     mini_margin = {top: 460, right: 80, bottom: 20, left: 40},
     main_width = 1300 - main_margin.left - main_margin.right,
     main_height = 525 - main_margin.top - main_margin.bottom,
@@ -15,9 +15,14 @@ var main_x0 = d3.scale.ordinal().rangeRoundBands([0, main_width-275], 0.2);
 var main_x1 = d3.scale.ordinal();
 
 // y is the fix time scal on the Y axis
+//var main_y  = d3.scale.linear().range([main_height, 0] );
 var main_y  = d3.scale.linear().range([main_height, 0] );
 
+
 var dateFormat = d3.time.format("%Y-%m-%d");
+var formatTime = d3.time.format("%X");
+var formatMillis = function(d) { return formatTime(new Date(2012, 0, 1, 0, 0, 0, d)); };
+var y_format = d3.format("1");
 
 // Define the X axis
 var main_xAxis = d3.svg.axis()
@@ -28,16 +33,7 @@ var main_xAxis = d3.svg.axis()
 // Define the Y axis
 var main_yAxis = d3.svg.axis()
     .scale(main_y)
-    .tickFormat(function(d) {
-      var milliseconds = d % 1000;
-      var seconds = Math.floor((d / 1000) % 60);
-      var minutes = Math.floor((d / (60 * 1000)) % 60);
- 
-      //var seconds = d / 1000;
-      //var minutes = seconds / 60;
-      //var hours = minutes / 60;
-      return minutes;
-    })
+    .tickFormat(function(d) { return d3.round((d / 1000 / 60), 0); } )
     .orient("left");
 
 // Define main svg element in #graph
@@ -54,13 +50,12 @@ var main = svg.append("g")
  */
 d3.json('fix_time_by_port.json', function(error, data) {
 
-  console.log(data);
 
   // This adds new elements to the data object
   data.result.forEach(function(d) {
     d.portfolio = d._id.portfolio;
     d.date = new Date(d._id.year, d._id.month-1, d._id.day);
-    d.vis = 1;
+    //d.vis = 1;
   });
 
   // define the axis domains
@@ -71,13 +66,18 @@ d3.json('fix_time_by_port.json', function(error, data) {
                           .sort(d3.ascending))
        .rangeRoundBands([0, main_x0.rangeBand() ], 0);
 
-  main_y.domain(d3.extent(data.result, function(d) { return d.buildFixTime; }));
+  main_y.domain([0, d3.max(data.result, function(d) { return d.buildFixTime; })]);
 
 
   // flatten out the data
   var nested = d3.nest()
       .key(function(d) { return d._id.portfolio; })
       .entries(data.result);
+
+  // Add the vis element to the nested data structure
+  nested.forEach(function(d) {
+      d.vis = 1;
+  });
 
   // Add the X axis
   main.append("g")
@@ -95,29 +95,21 @@ d3.json('fix_time_by_port.json', function(error, data) {
       .attr("y", 6)
       .attr("dy", ".71em")
       .style("text-anchor", "end")
-      .text("Build Fix Time (Hours)")
+      .text("Build Fix Time (Minutes)")
       .attr("class","y_label");
 
   var bar = main.selectAll(".bars")
       .data(nested)
     .enter().append("g")
-      .attr("class", function(d) { return d.key; })
-      .style("fill", function(d) { return color(d.key); } );
+      .attr("class", function(d) { return d.key + "-group"; })
+      .attr("fill", function(d) { return color(d.key); } );
 
   bar.selectAll("rect").append("rect")
-      .data(function(d) { console.log(d); return d.values; })
+      .data(function(d) { return d.values; })
     .enter().append("rect")
-      .attr("transform", function(d) { 
-          return "translate(" + main_x0(d.date) + ",0)"; 
-      })
-      .attr("width", function(d) {
-          if (d.vis=="1") {
-            return main_x1.rangeBand(); 
-          }
-          else {
-            return 0;
-          }
-      })
+      .attr("class", function(d) { return d.portfolio; })
+      .attr("transform", function(d) { return "translate(" + main_x0(d.date) + ",0)"; })
+      .attr("width", function(d) { return main_x1.rangeBand(); })
       .attr("x", function(d) { return main_x1(d.portfolio); })
       .attr("y", function(d) { return main_y(d.buildFixTime); })
       .attr("height", function(d) { return main_height - main_y(d.buildFixTime); });
@@ -143,7 +135,44 @@ d3.json('fix_time_by_port.json', function(error, data) {
       .attr("x",main_width-235)
       .attr("y", function(d,i) { return main_height-400 + (i*40); })
       .attr("stroke", function(d) { return color(d.key);})
-      .attr("fill", function(d) { return color(d.key); });
+      .attr("fill", function(d) {
+          if(d.vis=="1") {
+              return color(d.key);
+          }
+          else {
+              return "white";
+          }
+      })
+      .on("click", function(d) { 
+          if(d.vis=="1") {
+              d.vis="0";
+          }
+          else{
+              d.vis="1";
+          }
+
+          main.selectAll("." + d.key + "-group").transition()
+              //.attr("style", function(d) {
+              .attr("fill", function(d) {
+                  if (d.vis=="1") {
+                      return color(d.key);
+                  }
+                  else {
+                      return "white";
+                  }
+              });
+
+ 
+          legend.select("rect").transition()
+              .attr("fill", function(d) {
+                  if (d.vis=="1") {
+                      return color(d.key);
+                  }
+                  else {
+                      return "white";
+                  }
+              });
+      });
       
   
 });
