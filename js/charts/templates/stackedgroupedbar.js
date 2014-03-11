433f9ac8fb13e8a7fb465994a2fc5f11cc79b71a
Jim Goodspeed <james.goodspeed@libertymutual.com>
2014-03-11 16:34:23 -0400

    Commented out the Y axis brush update

--- js/charts/templates/stackedgroupedbar.js
+++ js/charts/templates/stackedgroupedbar.js
@@ -442,17 +442,18 @@ function stackedGroupedBarChart() {
 
                 main_x1.rangeRoundBands([0, main_x0.rangeBand()], 0);
 
-                console.log(main_xZoom.domain()[0]);
-                console.log(main_xZoom.domain()[1]);
+                //console.log(main_xZoom.domain()[0]);
+                //console.log(main_xZoom.domain()[1]);
 
-                console.log(nested);
+                //console.log(nested);
 
-                // filter the data to update the Y axis
+                /* filter the data to update the Y axis
                 var dataFiltered = data.result.filter(function(d) {
                     if((d.date >= main_xZoom.domain()[0]) && (d.date <= main_xZoom.domain()[1])) {
                         return yValue(d);
                     }
                 });
+		*/
 
                 //console.log(dataFiltered);
 
@@ -484,10 +485,10 @@ function stackedGroupedBarChart() {
 
 
 
-                main_y.domain([0, d3.max(dataFiltered.map(function(d) { return yValue(d); }))]);
+                //main_y.domain([0, d3.max(dataFiltered.map(function(d) { return yValue(d); }))]);
 
                 main.select("g.x.axis").call(main_xAxis);
-                main.select(".y.axis").transition().delay(500).call(main_yAxis);
+                //main.select(".y.axis").transition().delay(500).call(main_yAxis);
             } 
 
             // Get the max Y value
