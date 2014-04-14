7c55b6166c2299ad9af6fce156b3bb7f45a06ddd
Jim Goodspeed <james.goodspeed@libertymutual.com>
2014-04-14 13:09:27 -0400

    Using new bar chart template

--- js/charts/violationsbyportfolio.js
+++ js/charts/violationsbyportfolio.js
@@ -1,11 +1,10 @@
-var chart = stackedGroupedBarChart()
-    .x0(function(d) { return d.date; })
-    .x1(function(d) { return d.portfolio; })
+var chart = stackedBarChart()
+    .x(function(d) { return d.date; })
     .y(function(d) { return d.violations; })
+    .z(function(d) { return d.portfolio; })
     .yLabel("Count of Violations")
     .dimKey(function(d) { return d._id.portfolio; })
     .yTickFormat(function(d) { return d3.round((d), 1); })
-    //.colors(d3.scale.ordinal().range(["#5D5CD6","#5FD664","#FF7236","#D64041","#C53AD6"]));
     .colors(d3.scale.ordinal().range(["#D64041","#FF7236","#5FD664","#5D5CD6","#C53AD6"]));
 
 d3.json('data/violations_by_portfolio.json', function(data) {
