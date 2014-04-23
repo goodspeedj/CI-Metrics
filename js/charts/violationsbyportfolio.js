fc838a3d3f5405e2e665fedf8f87b118e9908d76
Jim Goodspeed <james.goodspeed@libertymutual.com>
2014-04-23 10:34:59 -0400

    Changed query to average so colors needed to be re-ordered

--- js/charts/violationsbyportfolio.js
+++ js/charts/violationsbyportfolio.js
@@ -5,7 +5,7 @@ var chart = stackedBarChart()
     .yLabel("Count of Violations")
     .dimKey(function(d) { return d._id.portfolio; })
     .yTickFormat(function(d) { return d3.round((d), 1); })
-    .colors(d3.scale.ordinal().range(["#D64041","#FF7236","#5FD664","#5D5CD6","#C53AD6"]));
+    .colors(d3.scale.ordinal().range(["#D64041","#FF7236","#5D5CD6","#5FD664","#C53AD6"]));
 
 d3.json('data/violations_by_portfolio.json', function(data) {
     d3.select("#graph")
