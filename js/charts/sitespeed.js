24539d3286deff966d39e92568b0905829425bc0
Jim Goodspeed <james.goodspeed@libertymutual.com>
2014-05-14 16:05:24 -0400

    Basic line chart for sitespeed.io data

---
+++ js/charts/sitespeed.js
@@ -0,0 +1,16 @@
+var chartName = "sitespeed";
+
+var chart = multiLineChart()
+    .x(function(d) { return d.date; })
+    .y(function(d) { return d.ruleScore; })
+    .yLabel("Score")
+    .dimKey(function(d) { return d._id.env; })
+    .yTickFormat(function(d) { return d3.round(d); })
+    .yScale(d3.scale.sqrt())
+    .color(d3.scale.category10());
+
+d3.json('data/sitespeed.json', function(data) {
+    d3.select("#graph")
+        .datum(data)
+        .call(chart);
+});
